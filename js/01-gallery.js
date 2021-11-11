c717f5f7c41ddccbbeda19fdcde4083e4d7d9c2a
Dmitriy <dmitriy.botsman@gmail.com>
2021-11-11 19:03:08 +0200

    copy

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+galleryItems.insertAdjacentHTML(afterbegin, string);
+
+// const ttt = galleryItems.map(elem => elem);
+//  document.createElement("img");
+// image.src = "elem.preview";
+// image.alt = "elem.description";
+// console.log(image);
+// console.log(ttt);
+
+
+
+
